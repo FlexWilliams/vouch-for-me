8589f3f7e11a8560e6dc4913e25e4a68ad91f897
cube <cube@so-icy-ent.biz>
2025-06-29 20:43:56 -0600

    fin task

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,45 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<header>
+	<a href="/"><h1>Vouch for Me</h1></a>
+</header>
+<main>
+	{@render children()}
+</main>
+<footer></footer>
+
+<style lang="scss">
+	header {
+		height: 25%;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+	}
+
+	h1 {
+		font-family: 'Chango-Regular', sans-serif;
+		padding: 2rem;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 2rem;
+		font-weight: bold;
+		color: #ede7f6;
+	}
+
+	main {
+		height: 50%;
+		width: 100%;
+		padding: 0 2rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+
+	footer {
+		height: 25%;
+		width: 100%;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,100 @@
-<header>
-	<h1>Vouch for Me</h1>
-</header>
-<main></main>
-<footer></footer>
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	let email: string = $state('');
+
+	let loading: boolean = $state(false);
+
+    // TODO: handle via svelte
+	async function handleFormSubmit(): Promise<void> {
+		console.log(email);
+
+		loading = true;
+
+        // TODO: impl during VOUCH-003, mocking for now
+		setTimeout(() => {
+			loading = false;
+		}, 2000);
+	}
+</script>
+
+<form class="form" name="sign-in-form" on:submit={() => handleFormSubmit()}>
+	<section class="form-field">
+		<label for="email"><span class="form-field-required">*</span>Email:</label>
+		<input id="email" type="email" bind:value={email} />
+	</section>
+	<button type="submit" class="submit-button" disabled={!email || loading}
+		>Send Magic Link
+		{#if loading}
+			<img
+				src="/images/gear.png"
+				alt="Loading..."
+				class="loading"
+				transition:fade={{ delay: 100, duration: 250 }}
+			/>
+		{/if}</button
+	>
+</form>
+
+<style lang="scss">
+	@import '../lib/styles/animations/spin.scss';
+
+	.form {
+		max-width: 36rem;
+		align-self: center;
+		width: 100%;
+		padding: 1rem;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.form-field {
+		display: flex;
+		flex-direction: column;
+		margin-bottom: 1rem;
+
+		label {
+			font-size: 1.25rem;
+			color: #dddddd;
+		}
+
+		input {
+			height: 2rem;
+			border-radius: 1rem;
+		}
+	}
+
+	.form-field-required {
+		color: red;
+		margin-right: 0.5rem;
+	}
+
+	.submit-button {
+		position: relative;
+		cursor: pointer;
+		border: none;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 1rem;
+		border-radius: 4rem;
+		padding: 0.5rem;
+		color: #ffffff;
+		background-color: #81d4fa;
+		font-weight: bold;
+
+		&:disabled {
+			background-color: gray;
+			cursor: not-allowed;
+		}
+	}
+
+	.loading {
+		width: 1.5rem;
+		height: 1.5rem;
+		position: absolute;
+		right: 1rem;
+
+		@include spin360;
+	}
+</style>
